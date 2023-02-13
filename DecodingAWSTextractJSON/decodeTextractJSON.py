e5d19de01aa685224a8fe0fb4c68215b44c084f2
Neil Thakur <56324725+neil-thakur@users.noreply.github.com>
2023-02-12 23:33:57 -0500

    Added code to outline primary JSON strucutre. Also referenced full file path for JSON file to avoid Errno 2.

--- DecodingAWSTextractJSON/decodeTextractJSON.py
+++ DecodingAWSTextractJSON/decodeTextractJSON.py
@@ -1,7 +1,13 @@
 import json
 
-f = open('analyzeDocResponse.json')
+f = open('/Users/neil-thakur/src/sdp-2022-nmt18004/DecodingAWSTextractJSON/analyzeDocResponse.json')
 data = json.load(f)
+
+print("PRIMARY JSON STRUCTURE:")
+for key in data:
+    print('\t', key)
+print()
+
 blocks = data['Blocks']
 
 for block in blocks:
