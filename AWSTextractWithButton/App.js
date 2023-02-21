d77253e8b6b509617f9b3bcc46070ad8ca92d4f9
Neil Thakur <56324725+neil-thakur@users.noreply.github.com>
2023-02-20 23:24:22 -0500

    Added code to new project, AWSTextractWithButton

--- AWSTextractWithButton/App.js
+++ AWSTextractWithButton/App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, ScrollView } from 'react-native';
+import { Textract } from 'aws-sdk/clients/textract';
 
 export default function App() {
+
+  const doSomething = () => {
+    
+    var AWS = require('aws-sdk');
+
+    var setCredentials = AWS.config.update({
+      accessKeyId: "AKIAQWTE7QBLRKHRBT6W",
+      secretAccessKey: "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",
+      region: "us-east-1"
+    });
+
+    var textract = new AWS.Textract({ region: 'us-east-1' });
+    
+    const params = {
+      Document: {
+        S3Object: {
+          Bucket: 'neil-thakur-test-bucket',
+          Name: 'S7 Class Schedule.png',
+        },
+      }
+      //FeatureTypes: ['TABLES', 'FORMS'],
+    };
+    
+    textract.detectDocumentText(params, function(err, data) {
+      if (err) {
+        console.log('Error', err);
+      } else {
+        console.log('Text detected:', data.Blocks.map(block => block.Text).join('\n'));
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>nmt18004 v1.0.0</Text>
+      <Text style={{fontSize: 18}}>
+        <Text style={{fontWeight: "bold"}}>Example: </Text>
+        <Text>Using Textract API on object in S3</Text>
+      </Text>
       <StatusBar style="auto" />
+      <TouchableHighlight onPress={doSomething}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Use Textract API</Text>
+        </View>
+      </TouchableHighlight>
+      <Text>Output is pasted below</Text>
     </View>
   );
 }
@@ -17,4 +60,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 35,
+    color: '#8f00ff',
+    fontWeight: 'bold'
+  },
+  button: {
+    padding: 10,
+    marginVertical: 15,
+    backgroundColor: '#0645AD'
+  },
+  buttonText: {
+    color: '#fff'
+  }
 });
