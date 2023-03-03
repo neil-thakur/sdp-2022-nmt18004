425508edad32a012a70daaedba22098530a5a10c
Neil Thakur <56324725+neil-thakur@users.noreply.github.com>
2023-03-02 20:33:31 -0500

    Adding updated AWS Textract code, omitting credentials

--- AWSTextractWithButton/App.js
+++ AWSTextractWithButton/App.js
@@ -1,54 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableHighlight, ScrollView } from 'react-native';
-import { Textract } from 'aws-sdk/clients/textract';
+import React from 'react';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
 
 export default function App() {
+
+  let [textractDump, setTextractDump] = React.useState('')
 
   const doSomething = () => {
     
     var AWS = require('aws-sdk');
 
     var setCredentials = AWS.config.update({
-      accessKeyId: "AKIAQWTE7QBLRKHRBT6W",
-      secretAccessKey: "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",
+      accessKeyId: "***",
+      secretAccessKey: "***",
       region: "us-east-1"
     });
 
     var textract = new AWS.Textract({ region: 'us-east-1' });
-    
+
     const params = {
       Document: {
         S3Object: {
           Bucket: 'neil-thakur-test-bucket',
-          Name: 'S7 Class Schedule.png',
+          //Name: 'Simple Table.png',
+          Name: 'S7 Class Schedule.png'
         },
       }
-      //FeatureTypes: ['TABLES', 'FORMS'],
+      //FeatureTypes: ['TABLES'],
     };
     
     textract.detectDocumentText(params, function(err, data) {
       if (err) {
         console.log('Error', err);
+        setTextractDump(err.toString())
       } else {
-        console.log('Text detected:', data.Blocks.map(block => block.Text).join('\n'));
+        console.log('Text detected:', data.Blocks.map(block => block.Text).join('\n\n'));
+        setTextractDump(data.Blocks.map(block => block.Text).join('\n\n'));
       }
     });
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>nmt18004 v1.0.0</Text>
-      <Text style={{fontSize: 18}}>
-        <Text style={{fontWeight: "bold"}}>Example: </Text>
-        <Text>Using Textract API on object in S3</Text>
-      </Text>
-      <StatusBar style="auto" />
-      <TouchableHighlight onPress={doSomething}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Use Textract API</Text>
-        </View>
-      </TouchableHighlight>
-      <Text>Output is pasted below</Text>
+    <View style = {styles.container}>
+      
+      <View style={{height: '25%', borderColor: 'black', borderBottomWidth: 5, borderTopWidth: 5, borderLeftWidth: 5, borderRightWidth: 5}}>
+        <Text style={styles.title}>nmt18004 v1.0.0</Text>
+        <Text></Text>
+        <Text style={{fontSize: 18}}>
+          <Text style={{fontWeight: "bold"}}>Example: </Text>
+          <Text>Using Textract API on object in S3</Text>
+        </Text>
+        <StatusBar style="auto" />
+        <Text></Text>
+        <TouchableHighlight onPress={doSomething} style ={{alignItems: 'center'}}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Use Textract API</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+
+      <View style={{height: '10%'}}>
+
+      </View>
+
+      <View style={{height: '25%', width: '75%'}}>
+        <ScrollView style={{borderColor: 'black', borderBottomWidth: 5, borderTopWidth: 5, borderLeftWidth: 5, borderRightWidth: 5}}>
+          <Text style={{textAlign: 'center'}}>{textractDump}</Text>
+        </ScrollView>
+      </View>
+
     </View>
   );
 }
@@ -59,16 +83,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%'
   },
   title: {
     fontSize: 35,
     color: '#8f00ff',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    textAlign: 'center'
   },
   button: {
     padding: 10,
-    marginVertical: 15,
-    backgroundColor: '#0645AD'
+    marginVertical: 5,
+    backgroundColor: '#0645AD',
+    width: '40%',
+    alignItems: 'center'
   },
   buttonText: {
     color: '#fff'
